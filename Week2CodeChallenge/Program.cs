f89c0cf142e8646e6b651a2c4cbd3dc1395ac3e7
Colin Pierce <cpierce46@gmail.com>
2014-06-27 12:54:48 -0600

    finished fizzbuzz

--- Week2CodeChallenge/Program.cs
+++ Week2CodeChallenge/Program.cs
@@ -10,11 +10,30 @@ namespace Week2CodeChallenge
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 20; i = i + 1)
+            {
+                FizzBuzz(i);
+            }
             Console.ReadKey();
         }
         static void FizzBuzz(int number)
         {
-
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
